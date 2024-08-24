a06d6e573208f7cf50b312418dc8345fae8e82f4
Md. Sohel Rana <sohelranarony55@gmail.com>
2024-08-25 00:25:10 +0600

    Create a test api route

--- api/index.js
+++ api/index.js
@@ -22,6 +22,9 @@ app.listen(3000, () => {
   console.log('Server is running on port 3000');
 })
 
+app.get('test/', (req, res) => {
+  res.json({message:'Api is working!'});
+})
 app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
+//app.use('/api/auth', authRoutes);
 
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,8 @@ import {test} from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.get('/test',test) 
+router.get('/test', test
+  
+);
 
 export default router;
